0723aa90fc7220d30e35cfcdd688e35624e9ea7c
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-12-26 20:38:22 +0800

    feat: finished the go and speed

--- motor/block/motor.js
+++ motor/block/motor.js
@@ -13,7 +13,7 @@ Blockly.Blocks.Mecanum_Car_Init = {
         this.setNextStatement(true);
         this.setTooltip(Blockly.Init_Car_Tip);
     }
-}
+};
 
 Blockly.Blocks.Mecanum_Wheel_Init = {
     init: function () {
@@ -23,5 +23,29 @@ Blockly.Blocks.Mecanum_Wheel_Init = {
         this.setPreviousStatement(true);
         this.setNextStatement(true);
         this.setTooltip(Blockly.Init_Wheel_Tip);
+    }
+};
+
+Blockly.Blocks.Mecanum_Car_Speed = {
+    init: function () {
+        this.setColour(Blockly.Blocks.motor.HUE3);
+        this.appendValueInput('Speed', Number)
+            .appendField(`${Blockly.Init_Speed} (0~4095)`)
+            .setCheck(Number);
+        this.setInputsInline(true);
+        this.setPreviousStatement(true);
+        this.setNextStatement(true);
+        this.setTooltip(Blockly.Init_Speed_Tip);
+    }
+};
+
+Blockly.Blocks.Init_Go = {
+    init: function () {
+        this.setColour(Blockly.Blocks.motor.HUE3);
+        this.appendDummyInput(Blockly.Init_Go)
+            .appendField(Blockly.Init_Go);
+        this.setPreviousStatement(true);
+        this.setNextStatement(true);
+        this.setTooltip(Blockly.Init_Go_Tip);
     }
 }
--- motor/generator/motor.js
+++ motor/generator/motor.js
@@ -7,7 +7,48 @@ Blockly.Arduino.Mecanum_Car_Init = function () {
 };
 
 Blockly.Arduino.Mecanum_Wheel_Init = function () {
-    Blockly.Arduino.definitions_['define_LeftWheelGo'] = "#define wheelLeftFrontGo 9";
-    Blockly.Arduino.definitions_['define_LeftWheelBack'] = "#define wheelLeftFrontBack 8";
+    Blockly.Arduino.definitions_['define_LeftFrontGo'] = "#define wheelLeftFrontGo 9";
+    Blockly.Arduino.definitions_['define_LeftFrontBack'] = "#define wheelLeftFrontBack 8";
+
+    Blockly.Arduino.definitions_['define_LeftRearGo'] = "#define wheelLeftRearGo 12";
+    Blockly.Arduino.definitions_['define_LeftRearBack'] = "#define wheelLeftRearGo 13";
+
+    Blockly.Arduino.definitions_['define_RightFrontGo'] = "#define wheelRightFrontGo 10";
+    Blockly.Arduino.definitions_['define_RightFrontBack'] = "#define wheelRightFrontBack 11";
+
+    Blockly.Arduino.definitions_['define_RightRearGO'] = "#define wheelRightRearGO 15";
+    Blockly.Arduino.definitions_['define_RightRearBack'] = "#define wheelRightRearBack 14";
+
+    Blockly.Arduino.setups_['init_pwm'] = "for (uint8_t i = 0; i < 16; i++)\n"
+        + "\t{\n"
+        + "\t\tpwm.setPwm(i, 0, 0)\n"
+        + "\t}"
     return '';
+}
+
+Blockly.Arduino.Mecanum_Car_Speed = function () {
+    let maxSpeed = Blockly.Arduino.valueToCode(this, 'Speed', Blockly.Arduino.ORDER_ASSIGNMENT) || 100;
+    if (maxSpeed < 0) {
+        maxSpeed = 0;
+    } else if (maxSpeed > 4095) {
+        maxSpeed = 4095;
+    }
+    Blockly.Arduino.definitions_['define_CarSpeed'] = `#define maxSpeed ${maxSpeed}`;
+    return '';
+}
+
+Blockly.Arduino.Init_Go = function () {
+    const code = "void go()\n"
+        + "{\n"
+        + "\tpwm.setPWM(wheelLeftFrontGo, 0, maxSpeed);\n"
+        + "\tpwm.setPWM(wheelLeftFrontBack, 0, 0);\n"
+        + "\tpwm.setPWM(wheelLeftRearGo, 0, maxSpeed);\n"
+        + "\tpwm.setPWM(wheelLeftRearBack, 0, 0);\n"
+        + "\tpwm.setPWM(wheelRightFrontGo, 0, maxSpeed);\n"
+        + "\tpwm.setPWM(wheelRightFrontBack, 0, 0);\n"
+        + "\tpwm.setPWM(wheelRightRearGO, 0, maxSpeed);\n"
+        + "\tpwm.setPWM(wheelRightRearBack, 0, 0);\n"
+        + "}"
+    Blockly.Arduino.definitions_['define_Go'] = code;
+    return "";
 }
