2517e3da24172f87e42b7cc9481bc535a4aa3322
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-12-30 22:10:43 +0800

    feat: zh-hans

--- motor/language/zh-hans.js
+++ motor/language/zh-hans.js
@@ -3,3 +3,24 @@ Blockly.Init_Car_Tip = "初始化小车驱动板";
 
 Blockly.Init_Wheel = "初始化麦克纳姆轮电机";
 Blockly.Init_Wheel_Tip = "初始化电机接口";
+
+Blockly.Init_Speed = "设置小车最高速度";
+Blockly.Init_Speed_Tip = "设置小车最高速度";
+
+Blockly.Init_SoftSerial = "初始化信道";
+Blockly.Init_SoftSerial_Tip = "初始化esp32与arduino信道";
+
+Blockly.Init_Go = "初始化前进方向";
+Blockly.Init_Go_Tip = "允许麦克纳姆轮小车前进";
+
+Blockly.Init_Back = "初始化后退方向";
+Blockly.Init_Back_Tip = "允许麦克纳姆轮小车后退";
+
+Blockly.Init_Right = "初始化向右方向";
+Blockly.Init_Right_Tip = "允许麦克纳姆轮小车向右";
+
+Blockly.Init_Left = "初始化向左方向";
+Blockly.Init_Left_Tip = "允许麦克纳姆轮小车向左";
+
+Blockly.Init_State_Machine = "初始化状态机";
+Blockly.Init_State_Machine_Tip = "初始化状态机";
