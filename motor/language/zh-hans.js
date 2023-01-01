3c4d40383ecf400aeca57479cdf10d5fe5afdeb1
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-01-01 18:07:46 +0800

    feat: make some zh-hans

--- motor/language/zh-hans.js
+++ motor/language/zh-hans.js
@@ -22,5 +22,8 @@ Blockly.Init_Right_Tip = "允许麦克纳姆轮小车向右";
 Blockly.Init_Left = "初始化向左方向";
 Blockly.Init_Left_Tip = "允许麦克纳姆轮小车向左";
 
+Blockly.Init_Stop = "初始化停止";
+Blockly.Init_Stop_Tip = "允许麦克纳姆轮小车停止";
+
 Blockly.Init_State_Machine = "初始化状态机";
 Blockly.Init_State_Machine_Tip = "初始化状态机";
